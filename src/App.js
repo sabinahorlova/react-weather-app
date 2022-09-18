e0c0641e5b6753419280b14cec8b6ffb65d854de
Sabina Horlova <sabinahorlova@gmail.com>
2022-09-18 11:22:45 +0300

    Added the GitHub rep URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://golden-marigold-44ac0f.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sabina Horlova
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/sabinahorlova/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GetHub
+        </a>
+      </footer>
+    </div>
+  );
 }
