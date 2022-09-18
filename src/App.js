2b7824968738a3f7776ca12b2a261ef482acb6da
Sabina Horlova <sabinahorlova@gmail.com>
2022-09-18 12:35:40 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://golden-marigold-44ac0f.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sabina Horlova
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/sabinahorlova/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GetHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://golden-marigold-44ac0f.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sabina Horlova
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/sabinahorlova/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GetHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return <div>Weather 2</div>;
+}
